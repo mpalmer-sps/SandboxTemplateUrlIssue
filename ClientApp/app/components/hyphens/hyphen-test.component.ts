2e7c3f483d67d16f0c00d42415afa587a7471444
Martin Palmer <martinp@quietpathtech.com>
2017-08-24 13:14:35 -0700

    Inital component added with the issue surfacing when a templateURL is used for the HTML file

---
+++ ClientApp/app/components/hyphens/hyphen-test.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hyphentest',
+    templateUrl: 'hyphen-test.component.html'
+    //template: '<div>Hello from Hyphen Test as a template property</div>'
+})
+export class HyphenComponent {
+
+    constructor() {
+        
+    }
+}
